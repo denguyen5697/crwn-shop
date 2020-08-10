a23d447426ba59084fe8b4bbefefc79d6b196c62
unknown <ntdvt197@gmail.com>
2020-08-11 01:08:05 +0700

    Deploy to gh-pages

--- src/components/page/checkout/checkout.component.jsx
+++ src/components/page/checkout/checkout.component.jsx
@@ -41,6 +41,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 12/20 - CVV: 123
+    </div>
+    <br />
       <StripeCheckoutButton price={total} />
     </div>
   );
