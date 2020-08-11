f30d4ea079c3d3e2427cea9ab9f7a7ec5f8f29b8
unknown <ntdvt197@gmail.com>
2020-08-12 02:24:26 +0700

    Updates

---
+++ precache-manifest.721248d11f53b6010a7053e3df02e21c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cfc476da12f26183558de1da6d10fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2d277f6e6547b612eb",
+    "url": "/static/css/main.5b528f1b.chunk.css"
+  },
+  {
+    "revision": "83f364d70a95367c4f9f",
+    "url": "/static/js/2.59cd79b5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.59cd79b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2d277f6e6547b612eb",
+    "url": "/static/js/main.ffb3c98d.chunk.js"
+  },
+  {
+    "revision": "854acf58065202342576",
+    "url": "/static/js/runtime-main.a9a15649.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crwn-shop/precache-manifest.6e543ae583d94abdb4e7adb60617f8bb.js"
+  "/precache-manifest.721248d11f53b6010a7053e3df02e21c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crwn-shop/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
